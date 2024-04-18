be3241578c6286e4a135afd981607cf299cdc605
its-jz <jialiang_zhou@brown.edu>
2024-04-17 23:00:15 -0400

    fix: coordinator script

--- ec2/coordinator.sh
+++ ec2/coordinator.sh
@@ -1,3 +1,3 @@
 output=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text | 
     paste -sd,)
-node server.js --workers $output --port 8080
+node ./coordinator/server.js --workers $output --port 8080

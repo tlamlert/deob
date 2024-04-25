ddef6dea9e466ad354308870ee6d5412c556b511
its-jz <jialiang_zhou@brown.edu>
2024-04-25 17:11:49 -0400

    feat: ec2/coordinator.sh

--- ec2/coordinator.sh
+++ ec2/coordinator.sh
@@ -1,3 +1,5 @@
-output=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text | 
-    paste -sd,)
-node ./coordinator/server.js --workers $output --port 8080
+current_ip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+ips=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text | 
+    tr '\t' '\n' | awk -v ip="$current_ip" '$1 != ip' | paste -sd,)
+
+tmux new-session -d -s worker "node /home/ubuntu/m6/coordinator/server.js --workers $ips --port 8080"

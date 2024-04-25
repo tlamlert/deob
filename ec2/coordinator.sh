82086e578efc09b826959df5545457c1ea460bcb
its-jz <jialiang_zhou@brown.edu>
2024-04-25 18:16:07 -0400

    fix: ec2 launch scripts

--- ec2/coordinator.sh
+++ ec2/coordinator.sh
@@ -1,5 +1,5 @@
 current_ip=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 ips=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].PublicIpAddress' --output text | 
     tr '\t' '\n' | awk -v ip="$current_ip" '$1 != ip' | paste -sd,)
-node /home/ubuntu/m6/coordinator/server.js --workers $ips --port 8080 --ip 0.0.0.0
-# tmux new-session -d -s worker "node /home/ubuntu/m6/coordinator/server.js --workers $ips --port 8080"
+
+tmux new-session -d -s c "node /home/ubuntu/m6/coordinator/server.js --workers $ips --port 8080 --ip 0.0.0.0 --workerPorts 8080"

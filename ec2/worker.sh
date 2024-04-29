591a3e80f089faa31eea7de92eb4c2cde6361857
its-jz <jialiang_zhou@brown.edu>
2024-04-29 15:17:34 -0400

    feat: pipe worker output to text file

--- ec2/worker.sh
+++ ec2/worker.sh
@@ -1,1 +1,1 @@
-tmux new-session -d -s w "node /home/ubuntu/m6/distribution.js --ip 0.0.0.0 --port 8080"
+tmux new-session -d -s w "node /home/ubuntu/m6/distribution.js --ip 0.0.0.0 --port 8080 > out.txt"
